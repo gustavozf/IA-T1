a4435798ebfe691484f228b478dfe10b34bf622e
narcizo <narcizo.gabriel2@gmail.com>
2018-05-03 11:36:04 -0300

    só pra dar um commit

--- investimentos.py
+++ investimentos.py
@@ -4,13 +4,13 @@ def melhorIndividuo(populacao):
 	'''
 	Funcao que retorna o melhor individuo de uma populacao
 	'''
-	return "TESTE: INDIVIDUO" 
+	return "TESTE: INDIVIDUO"
 
 def checkCriterioParada():
 	'''
-	Funcao que atualiza a populacao antiga com a nova. Elitista.
+	Funcao que atualiza o criterio de parada
 	'''
-	return False 
+	return False
 
 def atualizar(populacao, novaPopulacao):
 	'''
@@ -28,19 +28,19 @@ def reproduz(x, y):
 	'''
 	Reproduz um filho de "x" e "y"
 	'''
-	print "TESTE: REPRODUCAO"	
+	print "TESTE: REPRODUCAO"
 
 def fnFitness():
 	'''
 	Funcao que mede a adaptacao de um individuo
 	'''
-	print "TESTE: FITNESS"	
+	print "TESTE: FITNESS"
 
 def selecao(populacao):
 	'''
 	Selecao realizada por torneio ou roleta
 	'''
-	print "TESTE: SELECAO"	
+	print "TESTE: SELECAO"
 	fnFitness()
 
 def pequenaProbabilidadeAleatoria():
@@ -49,34 +49,35 @@ def pequenaProbabilidadeAleatoria():
 	Se este estiver presente entre 34 - 41, eh retornado True.
 	Caso contrario, False
 	'''
-	print "TESTE: pequenaProbabilidadeAleatoria"	
+	print "TESTE: pequenaProbabilidadeAleatoria"
 	return random.randint(1, 100) in range(34, 41)
 
 def buscaGenetico():
 	populacao = []
 	N = 1 # Quantidade de filhos gerados a cada iteracao.
 	criterioParada = True
-	
+
 	while criterioParada:
 		novaPopulacao = []
-		
+
 		for i in range(0, N):
 			x = selecao(populacao)
 			y = selecao(populacao)
 			filho = reproduz(x, y)
-			
+
 			if (pequenaProbabilidadeAleatoria()):
 				filho = mutacao(filho)
 
 			novaPopulacao.append(filho)
-		
+
 		atualizar(populacao, novaPopulacao)
-		
+
 		criterioParada = checkCriterioParada()
+		print "Criterio de parada = " +str(criterioParada)
 
 	return melhorIndividuo(populacao)
-		
+
 def buscaProporcao(opcao):
 	print "A opcao selecionada foi: {0}".format(opcao)
-	
+
 	buscaGenetico()
