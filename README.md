860d6738f271772e0a54b16066b94c8c97804100
gustavozf <gzf1996@gmail.com>
2018-05-13 20:07:35 -0300

    TAI MEU PAI

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 ## http://www.infomoney.com.br/mercados/empresas-bovespa
 ## http://cotacoes.economia.uol.com.br/acao/cotacoes-historicas.html?codigo=PETR4.SA&beginDay=1&beginMonth=1&beginYear=2014&endDay=31&endMonth=12&endYear=2015&page=2&size=200
 
-## WEG -> WEGE3
-## Cielo -> CIEL3
-## Ambev -> ABEV3
-## Lojas Americanas -> LAME4
-## Natura -> NATU3
-## Renner -> LREN3
-## Copel -> CPLE6
-## Amazon -> AMZO34
-## Klabin -> KLBN11
-## Vale -> VALE3
+{'americanas': [56.18, 18.9, 15.18], 
+'natura': [-32.04, -18.46, -9.48], 
+'cielo': [35.21, 5.0, -23.51], 
+'sanepar': [-37.66, -37.04, -4.44], 
+'weg': [33.58, 3.48, -20.35], 
+'bancodobrasil': [-8.92, -29.49, -38.7], 
+'ambev': [20.71, 14.32, -4.08], 
+'copel': [3.59, -30.57, -32.98], 
+'renner': [49.42, 18.61, -24.4], 
+'vale': [-70.01, -32.01, -23.83]}
--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-from investimentos import buscaProporcao
+from geraProporcao import buscaProporcao
 from dic import start
+import os
 
 def menu():
-    return input("Bem vindo ao ULTIMATE INVESTIDOR\n" +
+    return int(input("Bem vindo ao ULTIMATE INVESTIDOR\n" +
             "Para continuar selecione uma das opcoes de busca de investimento:\n"
             "\t 1 - OPCAO 1 (Gustavo Zanoni)\n" + 
             "\t 2 - OPCAO 2 (Mariana Soder)\n" + 
             "\t 3 - OPCAO 3 (Narcizo)\n" +
-            "> ")
+            "> "))
+
+def getValor():
+    return float(input("Insira o valor a ser investido:\n" +
+                        ">"))
 
 if __name__ == '__main__':
-    alg = menu()
-    print("Importando dados de 2014-2015...")
-    start()
-    print("Gerando proporcoes de investimentos...")
-    buscaProporcao(alg)
+    condicoes = [False, False]
+    valor = 0
+
+    os.system("clear")
+    while not condicoes[0]:
+        alg = menu()
+        condicoes[0] = alg in range(1, 4)
+
+        if not condicoes[0]:
+            os.system("clear")
+            print("Opcao inexistente! Tente novamente!")
+    
+    os.system("clear")
+    while not condicoes[1]:
+        valor = getValor()
+        condicoes[1] = valor >= 1000
+
+        if not condicoes[1]:
+            os.system("clear")
+            print("Valor insuficiente para investir em 10 empresas! Favor inserir um valor maior ou igual a R$1000,00")
+    
+    os.system("clear")
+    proporcao = buscaProporcao(valor)
+    
